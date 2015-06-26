3a03dc3f147bacf8dae4d64419e1991b9223726f
Oliver Gondža <ogondza@gmail.com>
2015-06-26 09:13:02 +0200

    Build automatically

--- clone.sh
+++ clone.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-java -jar "`dirname \"$0\"`"/target/config-cloner-*-jar-with-dependencies.jar $*
+dir="$( cd "$( dirname "$0" )" && pwd )"
+jar=`ls "$dir"/target/config-cloner-*-jar-with-dependencies.jar 2> /dev/null` || true
+if [ ! -f "$jar" ]; then
+  echo "Project not build, building it now..." >&2
+  mvn clean package -B -DskipTests=true -f $dir/pom.xml > /dev/null
+  jar=`ls "$dir"/target/config-cloner-*-jar-with-dependencies.jar`
+fi
+
+java -jar $jar "$@"
